50217720a8f40feb1f556479ae4813e253389e8d
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-01-14 15:42:00 +0100

    Initial commit

---
+++ app/routes/contacts.$contactId.destroy.jsx
@@ -0,0 +1,10 @@
+import { redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
+
+import { deleteContact } from "../data";
+
+export async function action({ params }) {
+    invariant(params.contactId, "Missing contactId param");
+    await deleteContact(params.contactId);
+    return redirect("/");
+}
